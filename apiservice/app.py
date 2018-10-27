5884fbfba429427de7ad371c0b13c22739070a54
Marcus Edwards <marcus.edwards@delphx.com>
2018-10-27 11:09:45 -0400

    add analysis queue

--- apiservice/app.py
+++ apiservice/app.py
@@ -1,9 +1,8 @@
 from flask import Flask, request
 import signal
-import sys
 from flask_cors import CORS
 from celery import Celery
-from auth import AuthGuard, REFRESH_TOKEN_KEY
+from .auth import AuthGuard, REFRESH_TOKEN_KEY
 from gevent import monkey
 from gevent.pywsgi import WSGIServer
 import json
@@ -26,19 +25,22 @@ rabbit.conf.task_default_queue = 'default'
 rabbit.conf.task_queues = (
     Queue('user', routing_key='user.tasks.#'),
     Queue('simulation', routing_key='simulation.tasks.#'),
+    Queue('analysis', routing_key='analysis.tasks.#'),
 )
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True, port=8080)
 
+
 @app.route('/<user_id>/teleportation/<measurements>', methods=['GET'])
-def teleportation():
+def teleportation(user_id, measurements):
     
     results = []
     for i in range(measurements):
         results.append(rabbit.send_task('simulation.tasks.teleportation', args=[user_id]).wait())
     
     return json.dumps(results)
+
 
 @app.route('/auth', methods=['POST', 'PUT'])
 def auth():
@@ -80,6 +82,7 @@ def auth():
         except (InvalidTokenError, ExpiredSignatureError) as ex:
             abort(401)
 
+
 @app.route('/user', methods=['GET', 'POST', 'PUT'])
 def users():
 
@@ -91,13 +94,15 @@ def users():
 
     elif request.method == 'PUT':
         return rabbit.send_task('tasks.update_users', args=[request.data]).wait()
-    
+
+
 @app.route('/user/<id>', methods=['GET'])
 def get_user(id):
 
     if request.method == 'GET':
         return rabbit.send_task('tasks.get_user', args=[id]).wait()
-    
+
+
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, _signal_handler)
     http_server = WSGIServer(('localhost', 5000), app.wsgi_app)
